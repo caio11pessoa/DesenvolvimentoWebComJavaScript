95b6cd657d05401fec1cc2a0e7295963c1ec2501
Caio Pessoa <caiopessoa1298@gmail.com>
2020-11-09 10:50:56 -0300

    lendo e escrevendo arquivo JSON

---
+++ node/arquivoLeitura.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+
+const caminho = __dirname + '/arquivo.json'
+
+// sincrono...
+const conteudo = fs.readFileSync(caminho, 'utf-8')
+console.log(conteudo)
+
+// assincrona...
+fs.readFile(caminho, 'utf-8', (err, conteudo)=>{
+    if(err){
+        console.log("deu ruim");
+    }else {
+        const config = JSON.parse(conteudo);
+        console.log(`${config.db.host}:${config.db.port}`)
+    }
+});
+
+const config = require('./arquivo.json')
+console.log(config.db);
+
+fs.readdir(__dirname, (err, arquivos)=> {
+    if(err){
+        console.log("deu ruim");
+    }else {
+        console.log('Conteúdo da pasta...')
+        console.log(arquivos, arquivos[1])
+    }
+})
